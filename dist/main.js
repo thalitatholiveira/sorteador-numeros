540af5a1e00273a553752ff832d9a310ff48a0dc
Thalita Oliveira <thalita.th.oliveira@gmail.com>
2021-03-25 20:09:36 -0300

    Add multiple sorted number functionality

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,27 @@
 function onClickButton() {
+    const amountSort = document.getElementById("amount").value;
     let min = document.getElementById("minimo").value;
     let max = document.getElementById("maximo").value;
 
     min = Math.ceil(min);
     max = Math.floor(max);
 
-    let sort = Math.floor(Math.random() * (max - min + 1)) + min;
-    document.getElementById("resultado").innerHTML = sort;
+    let sort = [];
+
+    for (let i = 0; i < amountSort; i++) {
+        const sortedNumber = Math.floor(Math.random() * (max - min + 1)) + min;   
+        sort.push(sortedNumber);     
+    }
+
+    let htmlSortedNumbers = '';
+
+    for (let i = 0; i < sort.length; i++) {
+        htmlSortedNumbers = `${htmlSortedNumbers}<p>${sort[i]}</p>`;
+    }
+
+    document.getElementById("result").innerHTML = `
+        <div class="result-container">
+            ${htmlSortedNumbers}
+        </div>
+    `;
 }
