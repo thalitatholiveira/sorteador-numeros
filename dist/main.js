848e5b54cffd0771f37863c5dcf447767a82690f
Thalita Oliveira <thalita.th.oliveira@gmail.com>
2021-04-03 21:36:38 -0300

    Number limit drawn added

--- dist/main.js
+++ dist/main.js
@@ -8,22 +8,28 @@ function onClickButton() {
 
     let sort = [];
 
-    for (let i = 0; i < amountSort; i++) {
-        const sortedNumber = Math.floor(Math.random() * (max - min + 1)) + min;   
-        sort.push(sortedNumber);     
+    if (amountSort > max) {
+        alert('Não é possível sortear uma quantidade de números maior do que o intervalo do sorteio. Tente novamente!')
+    } else if (amountSort > 1000) {
+        alert('A quantidade de números a sortear deve estar entre 1 e 1000. Tente novamente!')
+    } else {    
+        for (let i = 0; i < amountSort; i++) {
+            const sortedNumber = Math.floor(Math.random() * (max - min + 1)) + min;   
+            sort.push(sortedNumber);     
+        }
+    
+        let htmlSortedNumbers = '';
+    
+        for (let i = 0; i < sort.length; i++) {
+            htmlSortedNumbers = `${htmlSortedNumbers}<p>${sort[i]}</p>`;
+        }
+    
+        document.getElementById("result").innerHTML = `
+            <div class="result-container">
+                ${htmlSortedNumbers}
+            </div>
+        `;
     }
-
-    let htmlSortedNumbers = '';
-
-    for (let i = 0; i < sort.length; i++) {
-        htmlSortedNumbers = `${htmlSortedNumbers}<p>${sort[i]}</p>`;
-    }
-
-    document.getElementById("result").innerHTML = `
-        <div class="result-container">
-            ${htmlSortedNumbers}
-        </div>
-    `;
 }
 
 function clearButton() {
